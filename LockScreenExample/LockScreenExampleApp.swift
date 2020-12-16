bbc17afa76240cd7fdeb7efafd4fe134240615c5
Guilherme Barros <hello@gbarros.dev>
2020-12-15 22:49:04 -0300

    Initial Commit

---
+++ LockScreenExample/LockScreenExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockScreenExampleApp.swift
+//  LockScreenExample
+//
+//  Created by Guilherme Barros on 15/12/20.
+//
+
+import SwiftUI
+
+@main
+struct LockScreenExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
